7549faae1aec5520d5f920ed9e6855fb4d9673ff
kalpakprajapati <kalpakprajapati15@gmail.com>
2021-08-08 20:04:26 +0530

    final app

---
+++ app.js
@@ -0,0 +1,110 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const array = require("lodash");
+const mongoose =  require("mongoose");
+
+mongoose.connect("Enter your mongo srv here/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const postSchema = new mongoose.Schema({
+  postTitle: String,
+  postBody: String
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+const post = new Post({
+  postTitle: "Hello",
+  postBody: homeStartingContent
+});
+
+const app = express();
+
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res){
+  Post.find(async function(err, foundItems){
+    if(foundItems.length===0){
+      await post.save();
+      res.redirect("/");
+    }else{
+      res.render("home", {homeContent: foundItems});
+    }
+  })
+
+})
+app.get("/about",function(req,res){
+  res.render("about", {aboutContent: aboutContent});
+})
+app.get("/contact",function(req,res){
+  res.render("contact", {contactContent: contactContent});
+})
+app.get("/compose",function(req,res){
+  res.render("compose");
+})
+
+app.get("/posts/:para", function(req,res){
+  var check = array.lowerCase(req.params.para);
+
+  Post.find(function(err, foundItems){
+    for(var i =0; i<foundItems.length;i++){
+      const lowerStored = array.lowerCase(foundItems[i].postTitle);
+      if(lowerStored===check){
+      res.render("post", {postTitle: foundItems[i].postTitle, postBody: foundItems[i].postBody});
+    }
+    }
+  })
+
+
+});
+
+app.post("/compose", async function(req,res){
+
+  const data =new Post({
+    postTitle: req.body.newPost,
+    postBody: req.body.postBody
+  });
+
+await data.save();
+ res.redirect("/");
+})
+
+
+
+app.post("/delete", function(req,res){
+  const deleteItem = req.body.button;
+  Post.deleteOne({postTitle: deleteItem}, async function(err){
+    if(err){
+      console.log(err);
+    }else{
+    await  console.log("Deleted item with title" + deleteItem);
+    }
+  });
+ res.redirect("/");
+});
+
+
+
+
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("server has started successfully");
+});
